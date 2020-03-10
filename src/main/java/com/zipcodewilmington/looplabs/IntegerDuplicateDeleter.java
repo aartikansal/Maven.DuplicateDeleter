f0869fa29bc87f61056e20f921adab3fe556d720
aartikansal <aartilumb@gmail.com>
2020-03-10 05:27:44 -0400

    cmpleted

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,76 @@
 package com.zipcodewilmington.looplabs;
+        import java.lang.reflect.Array;
+        import java.util.Arrays;
 
 /**
  * Created by leon on 1/29/18.
+ *
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] value = new Integer[0];
+        Integer numberOfOccurance = 0;
+        Integer[] myArray;
+
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array.length; j++) {
+                if (array[i] == array[j]) {
+                    numberOfOccurance++;
+                }
+
+            }
+            if (numberOfOccurance < maxNumberOfDuplications) {
+
+
+                myArray = Arrays.copyOf(value, value.length + 1);
+                myArray[myArray.length - 1] = array[i];
+
+                value = myArray;
+
+
+            }
+            numberOfOccurance = 0;
+        }
+
+        return value;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] value = new Integer[0];
+        Integer numberOfOccurance = 0;
+        Integer[] myArray;
+
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array.length; j++) {
+                if (array[i] == array[j]) {
+                    numberOfOccurance++;
+                }
+
+            }
+            if (numberOfOccurance != exactNumberOfDuplications) {
+
+
+                myArray = Arrays.copyOf(value, value.length + 1);
+                myArray[myArray.length - 1] = array[i];
+
+                value = myArray;
+
+
+            }
+            numberOfOccurance = 0;
+        }
+
+
+        return value;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,73 @@
 package com.zipcodewilmington.looplabs;
-
+import java.util.Arrays;
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
+
+        import java.util.Arrays;
+
+/**
+ * Created by leon on 1/28/18.
+ *
+ * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] value = new String[0];
+        Integer numberOfOccurance = 0;
+        String[] myArray;
+
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    numberOfOccurance++;
+                }
+
+            }
+            if (numberOfOccurance < maxNumberOfDuplications) {
+
+
+                myArray = Arrays.copyOf(value, value.length + 1);
+                myArray[myArray.length - 1] = array[i];
+
+                value = myArray;
+
+
+            }
+            numberOfOccurance = 0;
+        }
+
+        return value;
+
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] value = new String[0];
+        Integer numberOfOccurance = 0;
+        String[] myArray;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    numberOfOccurance++;
+                }
+            }
+            if (numberOfOccurance != exactNumberOfDuplications) {
+
+                myArray = Arrays.copyOf(value, value.length + 1);
+                myArray[myArray.length - 1] = array[i];
+                value = myArray;
+            }
+            numberOfOccurance = 0;
+        }
+
+
+        return value;
+    }
 }
